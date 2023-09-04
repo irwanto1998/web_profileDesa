0b2ec5b4a3d8f544c33f9cfe184030a4acdcbbb2
irwanto1998 <rioirwanto9@gmail.com>
2023-09-04 09:35:45 +0700

    Perbaikan

--- resources/views/components1/footer1.blade.php
+++ resources/views/components1/footer1.blade.php
@@ -48,6 +48,18 @@
 
       </div>
     </div>
+    <div class="container mt-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Desa Ampera Raya</span></strong>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/impact-bootstrap-business-website-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">MHS UM Pontianak KKU Kel.71 2023</a>
+      </div>
+    </div>
 
   </footer><!-- End Footer -->
   <!-- End Footer -->
